a32fb5d2d825da64606cf4873a11c6ac8278d993
sarunyou <sarunyouwhang11@gmail.com>
2015-12-10 00:16:34 +0700

    can light ywc letter

--- ctrl.js
+++ ctrl.js
@@ -1,4 +1,4 @@
-var alphabet = [
+var letters = [
   ["I", "T", "L", "I", "S", "B", "F", "A", "M", "P", "M"],
   ["A", "C", "Q", "U", "A", "R", "T", "E", "R", "D", "C"],
   ["T", "W", "E", "N", "T", "Y", "F", "I", "V", "E", "X"],
@@ -11,19 +11,23 @@ var alphabet = [
   ["T", "E", "N", "S", "E", "O", "C", "L", "O", "C", "K"]
 ];
 var text = [];
-
-for (i = 0; i < alphabet.length; i++) {
+var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+function generateLetters() {
+   var letter = possible.charAt(Math.floor(Math.random() * possible.length))
+    // text.push();
+}
+for (i = 0; i < letters.length; i++) {
   var isStart = true
-      text.push("<span>" + alphabet[i][0] + "</span>");
-      text.push("<span>" + alphabet[i][1] + "</span>");
-  for (var j = 2; j < alphabet[i].length; j++) {
-      text.push("<span>" + alphabet[i][j] + "</span>");
-    if (alphabet[i][j-2] == "Y" && alphabet[i][j-1] == "W" && alphabet[i][j] == "C" && isStart) {
+      text.push("<span>" + letters[i][0] + "</span>");
+      text.push("<span>" + letters[i][1] + "</span>");
+  for (var j = 2; j < letters[i].length; j++) {
+      text.push("<span>" + letters[i][j] + "</span>");
+    if (letters[i][j-2] == "Y" && letters[i][j-1] == "W" && letters[i][j] == "C" && isStart) {
       console.log('hey i get it');
         text.splice(-3,3);
         for (var g = j-2; g <= j; g++) {
-          console.log('i push ' + alphabet[i][g]);
-          text.push("<span class='light'>" + alphabet[i][g] + "</span>");
+          console.log('i push ' + letters[i][g]);
+          text.push("<span class='light'>" + letters[i][g] + "</span>");
         }
 
     }
