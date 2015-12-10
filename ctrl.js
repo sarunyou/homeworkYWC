4b28b8531ce7930dfa5afca6ac1e845c9b228a4d
sarunyou <sarunyouwhang11@gmail.com>
2015-12-10 13:11:47 +0700

    first version finished

--- ctrl.js
+++ ctrl.js
@@ -1,40 +1,54 @@
-var letters = [
-  ["I", "T", "L", "I", "S", "B", "F", "A", "M", "P", "M"],
-  ["A", "C", "Q", "U", "A", "R", "T", "E", "R", "D", "C"],
-  ["T", "W", "E", "N", "T", "Y", "F", "I", "V", "E", "X"],
-  ["H", "A", "L", "F", "B", "T", "E", "N", "F", "T", "O"],
-  ["P", "A", "S", "T", "E", "R", "U", "N", "I", "N", "E"],
-  ["O", "N", "E", "S", "Y", "W", "C", "H", "R", "E", "E"],
-  ["F", "O", "U", "R", "F", "I", "V", "E", "T", "W", "O"],
-  ["E", "I", "G", "H", "T", "E", "L", "E", "V", "E", "N"],
-  ["S", "E", "V", "E", "N", "T", "W", "E", "L", "V", "E"],
-  ["T", "E", "N", "S", "E", "O", "C", "L", "O", "C", "K"]
-];
-var text = [];
 var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numRow = 10;
+var numCol = 11;
+text = []
+
+
 function generateLetters() {
-   var letter = possible.charAt(Math.floor(Math.random() * possible.length))
-    // text.push();
-}
-for (i = 0; i < letters.length; i++) {
-  var isStart = true
-      text.push("<span>" + letters[i][0] + "</span>");
-      text.push("<span>" + letters[i][1] + "</span>");
-  for (var j = 2; j < letters[i].length; j++) {
-      text.push("<span>" + letters[i][j] + "</span>");
-    if (letters[i][j-2] == "Y" && letters[i][j-1] == "W" && letters[i][j] == "C" && isStart) {
-      console.log('hey i get it');
-        text.splice(-3,3);
-        for (var g = j-2; g <= j; g++) {
-          console.log('i push ' + letters[i][g]);
-          text.push("<span class='light'>" + letters[i][g] + "</span>");
-        }
-
+  for (var row = 0; row < numRow; row++) {
+    for (var column = 0; column < numCol; column++) {
+      var letter = possible.charAt(Math.floor(Math.random() * possible.length));
+      text.push("<span>" + letter + "</span>");
     }
-
-
+    text.push("<br>");
   }
-  text.push("<br>");
-}
+};
 
-document.getElementById("test").innerHTML = text.join(" ");
+function changeRowColToIndex(row, col) {
+  return (row * 12) + col;
+};
+
+function createLightWordVertical(word) {
+  var indexWord = 0;
+  var useCol = Math.floor(Math.random() * numCol);
+  var useRow = Math.floor(Math.random() * (numRow - word.length));
+  for (var row = useRow; row < useRow + word.length; row++) {
+    text[changeRowColToIndex(row, useCol)] = "<span class='light'>" + word.charAt(indexWord) + "</span>";
+    indexWord += 1;
+  }
+};
+
+function createLightWordHorizontal(word) {
+  var indexWord = 0;
+  var useCol = Math.floor(Math.random() * (numCol - word.length));
+  var useRow = Math.floor(Math.random() * (numRow));
+  console.log('useCol =' + useCol + ' useRow = ' + useRow);
+  for (var col = useCol; col < useCol + word.length; col++) {
+    text[changeRowColToIndex(useRow, col)] = "<span class='light'>" + word.charAt(indexWord) + "</span>";
+    indexWord += 1;
+  }
+};
+
+function genNewLetters() {
+  wordLight = document.getElementById("wordInput").value.toUpperCase();
+  text = []
+  generateLetters();
+  if (Math.random() > 0.5)
+    createLightWordVertical(wordLight);
+  else
+    createLightWordHorizontal(wordLight);
+  document.getElementById("test").innerHTML = text.join(" ");
+
+
+}
+genNewLetters();
